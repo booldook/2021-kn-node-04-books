3954716a459a670234e7777fd0eb93c7fa750ce0
booldook <booldook@gmail.com>
2021-04-03 13:48:19 +0900

    multer-conn 완성

--- modules/multer-conn.js
+++ modules/multer-conn.js
@@ -4,23 +4,36 @@ const fs = require('fs-extra')
 const { v4: uuid4 } = require('uuid')
 const moment = require('moment')
 
+const allowImgExt = ['jpg', 'jpeg', 'png', 'gif']
+const allowFileExt = ['txt', 'pdf', 'ppt', 'pptx', 'doc', 'docx', 'zip', 'xls', 'xlsx']
+const allowExt = [...allowImgExt, ...allowFileExt]
+
 const destCb = (req, file, cb) => {
-	try {
-		let folder = path.join(__dirname, '../storages', moment().format('YYYYMMDD_HH'))
-		fs.ensureDirSync(folder)
-		console.log(cb)
-		cb(null, folder)
-	}
-	catch(err) {
-		cb(err)
-	}
+	let folder = path.join(__dirname, '../storages', moment().format('YYYYMMDD_HH'))
+	fs.ensureDirSync(folder)	// 폴더가 존재하면 패스, 없으면 만들고...(fs-extra)
+	cb(null, folder)
 }
 
 const fileCb = (req, file, cb) => {
-
+	let ext = path.extname(file.originalname)	//.jpg
+	let name = moment().format('YYYYMMDD_HH') + '-' + uuid4() + ext
+	cb(null, name)
 }
 
+const fileFilter = (req, file, cb) => {
+	let ext = path.extname(file.originalname).substr(1).toLowerCase()
+	if(allowExt.includes(ext)) {
+		cb(null, true)
+	}
+	else {
+		req.banExt = ext
+		cb(null, false)
+	}
+}
+
+const limits = { fileSize: 102400000 }
+
 const storage = multer.diskStorage({ destination: destCb, filename: fileCb })
-const upload = multer({ storage })
+const upload = multer({ storage, fileFilter, limits })
 
 module.exports = { upload }
--- routes/multer-router.js
+++ routes/multer-router.js
@@ -6,12 +6,22 @@ const path = require('path')
 var { upload }  = require('../modules/multer-conn')
 const pug = { title: '업로드', file: 'multer' }
 
-router.get('/create', async (req, res, next) => {
+router.get('/create', async (req, res, next) => {1
 	res.render('multer/create', pug)
 })
 
 router.post('/save', upload.single('upfile'), (req, res, next) => {
-	res.json({ file: req.file })
+	if(req.banExt) {
+		res.send('허용되지 않은 파일확장자 입니다.')
+	}
+	else {
+		if(req.file) {
+			res.json({ file: req.file })
+		}
+		else {
+			res.send('파일없이 저장')
+		}
+	}
 })
 
 module.exports = router
