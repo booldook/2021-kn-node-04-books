993b3b6e4982998feec9d35465945abfd8dad5b4
booldook <booldook@gmail.com>
2021-04-03 12:17:59 +0900

    ...

--- modules/multer-conn.js
+++ modules/multer-conn.js
@@ -4,8 +4,23 @@ const fs = require('fs-extra')
 const { v4: uuid4 } = require('uuid')
 const moment = require('moment')
 
-const storage = {}
+const destCb = (req, file, cb) => {
+	try {
+		let folder = path.join(__dirname, '../storages', moment().format('YYYYMMDD_HH'))
+		fs.ensureDirSync(folder)
+		console.log(cb)
+		cb(null, folder)
+	}
+	catch(err) {
+		cb(err)
+	}
+}
 
+const fileCb = (req, file, cb) => {
+
+}
+
+const storage = multer.diskStorage({ destination: destCb, filename: fileCb })
 const upload = multer({ storage })
 
 module.exports = { upload }
