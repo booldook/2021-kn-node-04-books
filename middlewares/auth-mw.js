d4e9ac2c3983904763830c38eeff89a02f63d778
booldook <booldook@gmail.com>
2021-04-24 17:29:54 +0900

    ...

--- middlewares/auth-mw.js
+++ middlewares/auth-mw.js
@@ -1,14 +1,14 @@
 const { alert } = require('../modules/util')
 
 const isUser = (req, res, next) => {
-	if(req.session && req.session.user) next()
+	if(req.user) next()
 	else {
 		res.send(alert('권한이 없습니다. 로그인 후 사용하세요.', '/auth/login'))
 	}
 }
 
 const isGuest = (req, res, next) => {
-	if(!req.session || !req.session.user) next()
+	if(!req.user) next()
 	else {
 		res.send(alert('비회원만 사용가능 합니다.', '/'))
 	}
